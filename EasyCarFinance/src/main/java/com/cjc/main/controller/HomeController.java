2535471d73898af4a7dc87ef505df0eea7790a01
narkhedepratik <98662996+narkhedepratik@users.noreply.github.com>
2024-12-27 09:58:27 +0530

    initial project structure

---
+++ EasyCarFinance/src/main/java/com/cjc/main/controller/HomeController.java
@@ -0,0 +1,16 @@
+ package com.cjc.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
+
+@RestController
+ public class HomeController{
+	 
+	@GetMapping("/")
+	public String testApi()
+	{
+		return "I am running fine";
+	}
+	 
+ }
---
+++ EasyCarFinance/src/test/java/com/cjc/main/EasyCarFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCarFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
