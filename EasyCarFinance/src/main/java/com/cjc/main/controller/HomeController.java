ddf7a60d83d2c05db63cc667759d33ba309d5558
narkhedepratik <98662996+narkhedepratik@users.noreply.github.com>
2024-12-28 11:45:53 +0530

    delete API

--- EasyCarFinance/src/main/java/com/cjc/main/controller/HomeController.java
+++ EasyCarFinance/src/main/java/com/cjc/main/controller/HomeController.java
@@ -1,6 +1,9 @@
  package com.cjc.main.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.service.annotation.GetExchange;
 
@@ -19,5 +22,11 @@ import org.springframework.web.service.annotation.GetExchange;
 		
 		return "No enquiries yet";
 	}
+	 
+	 @DeleteMapping("/delete-user/{userId}")
+	 public void onDeleteuser(@PathVariable("userId") int userId)
+	 {
+		 // deleting user......
+	 }
 	 
  }
